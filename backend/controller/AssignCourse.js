250b6805a63fd06c6ba8e0cd766996f985488f35
MdNadimUddin01 <mdnadimuddin00786@gmail.com>
2024-09-30 23:07:49 +0530

    course assign without payment integration

---
+++ backend/controller/AssignCourse.js
@@ -0,0 +1,32 @@
+const { Course } = require("../model/Course");
+const { User } = require("../model/User");
+
+exports.assignCourse = async(req , res) => {
+
+    try{
+
+        const {courseId} = req.body;
+        const id = req.id;
+
+        const courseDetails = await Course.findByIdAndUpdate({_id:courseId} , {
+            $push:{
+                studentEnrolled:id
+            }
+        })
+
+        const updateUser = await User.findByIdAndUpdate({_id:id} ,{
+            $push:{
+                courses:courseDetails._id
+            }
+        })
+
+        return res.status(200).json({
+            success:true,
+            message:"Course added",
+            updateUser,
+        })
+        
+    }catch(error){
+        return next(error)
+    }
+} 
