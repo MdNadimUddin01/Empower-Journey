f221926eb2cc925dbf5a64d31a054d0bc94f4e11
MdNadimUddin01 <mdnadimuddin00786@gmail.com>
2024-10-08 01:06:34 +0530

    update multer configuration

--- backend/routes/subSection.js
+++ backend/routes/subSection.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { createSubsection, updateSubSection, deleteSubSection } = require("../controller/SubSection");
 const { verifyToken, isInstructor } = require("../middleware/Auth");
+const { upload } = require("../middleware/multer");
 const route = express.Router();
 
 
-route.post("/createSubsection", verifyToken, isInstructor,createSubsection)
+route.post("/createSubsection", verifyToken, isInstructor, upload.fields([{name:"videoFile" , maxCount:1} , {name:"otherFile" , maxCount:1}]) ,createSubsection)
 route.put("/updateSubSection" ,verifyToken, isInstructor, updateSubSection)
 route.delete("/deleteSubSection" ,verifyToken, isInstructor, deleteSubSection)
 
